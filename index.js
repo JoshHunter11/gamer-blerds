9aa53884161fdc5051a4e3aa403cc1d6c31eea86
JoshHunter11 <weirdstuff1105@gmail.com>
2023-02-08 01:57:39 +0000

    add calculateDistance method

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-(function(window, _) {
-    window.myLibraryName = window.myLibraryName || {
-      numz: {},
-      phyz: {},
-    };
-  }(window, window._));
+(function (window, _) {
+  window.myLibraryName = window.myLibraryName || {
+    numz: {},
+    phyz: {
+
+      /**
+      * 
+      * @param {number} pointA a point on a grid/screen
+      * @param {number} pointB another point on a grid/screen
+      * 
+      * @param {number} pointB.x x value of point b
+      * @param {number} pointB.y y value of point b 
+      * 
+      * @param {number} pointA.x x value of point a 
+      * @param {number} pointA.y y value of point b
+      * 
+      * @returns {number} the square of both distances adde 
+      */
+
+      getDistance(pointA, pointB) {
+        const
+          distanceX = pointB.x - pointA.x,
+          distanceY = pointB.y - pointA.y,
+          distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+        return distance;
+      }
+
+    },
+  };
+}(window, window._));
+
+
+
